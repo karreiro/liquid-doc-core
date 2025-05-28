ee1ed5fbbdfc6ddf250c49cfa28fc8ca61069492
Josh Faigan <josh.faigan@shopify.com>
2025-05-28 14:01:03 -0400

    adding ohm route

--- app.rb
+++ app.rb
@@ -12,6 +12,10 @@ get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
 
+get '/ohm' do
+  send_file File.join(settings.public_folder, 'ohm.html')
+end
+
 # Health check endpoint
 get '/health' do
   content_type :json
