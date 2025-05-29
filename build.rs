9b433ff4278f30d6e97ac80ea33cc12e92d07291
Josh DeGraw <18509575+josh-degraw@users.noreply.github.com>
2025-05-29 14:41:54 -0400

    Fix build script, generate new test fixtures

--- build.rs
+++ build.rs
@@ -5,70 +5,41 @@ use std::path::Path;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("test_files.rs");
+    let dest_path = Path::new(&out_dir).join("fixtures_macro.rs");
 
-    let dir_path = "web/fixtures";
-    let entries = fs::read_dir(dir_path)
-        .unwrap()
+    let fixtures_dir = Path::new("web/fixtures");
+    let entries = fs::read_dir(fixtures_dir)
+        .expect("Failed to read fixtures directory")
         .filter_map(|entry| {
             let entry = entry.ok()?;
             let path = entry.path();
-            if path.is_file() && path.extension() == Some(std::ffi::OsStr::new("liquid")) {
-                path.file_stem()
-                    .and_then(|s| s.to_str())
-                    .map(|s| s.to_owned())
+            if path.is_file() && path.extension()?.to_str()? == "liquid" {
+                path.file_stem()?.to_str().map(|s| s.to_owned())
             } else {
                 None
             }
         })
         .collect::<Vec<_>>();
 
-    // Generate multiple useful macros
-    let macro_definition = format!(
+    // Generate the macro
+    let macro_content = format!(
         r#"
-// Macro to iterate over each test file name
 #[macro_export]
 macro_rules! for_each_fixture_file {{
-    ($macro:ident) => {{
+    ($callback:ident) => {{
         {}
-    }}
-}}
-
-// Macro that expands to an array of all fixture file names
-#[macro_export]
-macro_rules! fixture_file_names {{
-    () => {{
-        &[{}]
-    }}
-}}
-
-// Macro that expands to an array of all fixture file paths
-#[macro_export]
-macro_rules! fixture_file_paths {{
-    () => {{
-        &[{}]
-    }}
+    }};
 }}
 "#,
         entries
             .iter()
-            .map(|name| format!("        $macro!({:?});", name))
+            .map(|name| format!("$callback!({:?});", name))
             .collect::<Vec<_>>()
-            .join("\n"),
-        entries
-            .iter()
-            .map(|name| format!("{:?}", name))
-            .collect::<Vec<_>>()
-            .join(", "),
-        entries
-            .iter()
-            .map(|name| format!("{:?}", format!("web/fixtures/{}.liquid", name)))
-            .collect::<Vec<_>>()
-            .join(", ")
+            .join("\n        ")
     );
 
-    fs::write(&dest_path, macro_definition).unwrap();
+    fs::write(&dest_path, macro_content).unwrap();
 
-    // Rebuild if the directory changes
-    println!("cargo:rerun-if-changed={}", dir_path);
+    // Rebuild if fixtures directory changes
+    println!("cargo:rerun-if-changed=web/fixtures");
 }
